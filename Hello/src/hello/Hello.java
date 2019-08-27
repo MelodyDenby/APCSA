f238b23490f05a92e88ce51c3116994790154d7c
Melody Denby <20014022@hartlandschools.us>
2019-08-27 17:19:24 -0400

    homework

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Hello {
+
+    public static void hello() {
+        System.out.println("Hello World!");
+    }  
+    public static void main(String[] args) {
+        hello();
+    }
+    
+
+}
---
+++ MuchBetter/src/muchbetter/MuchBetter.java
@@ -0,0 +1,14 @@
+
+package muchbetter;
+
+public class MuchBetter {
+    public static void main(String[] args) {
+        System.out.println("A \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences.\"");
+        System.out.println("Also, \"\" represents an empty string.");
+        System.out.println("Don't forget: use  \\\" instead of \" !");
+        System.out.println("'' is not the same as \"");
+    }
+    
+}
