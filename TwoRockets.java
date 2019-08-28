37f18ade5b54410346f62a49844ea652d5eca62b
MelodyDenby <20014022@hartlandschools.us>
2019-08-28 08:11:59 -0400

    Create TwoRockets.java

---
+++ TwoRockets.java
@@ -0,0 +1,12 @@
+public class TwoRockets(){
+   public static void main(String[] args){
+      System.out.println("  /\\     /");
+      System.out.println(" /  \\   /");
+      System.out.println("/    \\ /");
+   }
+    public static void across(){
+    
+      }
+    public static void top(){
+    
+    }
