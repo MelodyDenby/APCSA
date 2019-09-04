ab109fe76aef015b645a5821a270c0b79b0ad6ca
Melody Denby <20014022@hartlandschools.us>
2019-09-04 16:46:33 -0400

    Create OldLady.java

---
+++ OldLady/src/oldlady/OldLady.java
@@ -0,0 +1,20 @@
+package oldlady;
+
+public class OldLady {
+    public static void main(String[] args) {
+        para1();
+    }
+    public static void basic() {
+        System.out.print("There was an old lady who swallowed a ");
+    }
+    public static void paraEnd(){
+        System.out.println("I don't know why she swallowed that fly,\nPerhaps she'll die.");
+    }
+    public static void para1(){
+        basic();
+        System.out.println("spider,\nThat wriggled and iggled and jiggled inside her./nShe swallowed the spider to catch the fly,");
+        paraEnd();
+        
+        
+    }
+}
