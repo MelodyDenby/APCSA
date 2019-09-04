8d8165de39bb9b7facb41215410ebea9ac3a4d0b
MelodyDenby <20014022@hartlandschools.us>
2019-09-04 08:09:44 -0400

    Create Mississippi.java

---
+++ Mississippi.java
@@ -0,0 +1,22 @@
+public class Mississippi {
+   public static void main(String args[]){
+      letterM();
+      TwoISS();
+      letterI();
+      letterP();
+      letterP();
+      letterI();
+   }
+   public static void letterM(){
+   System.out.println("M     M\nMM   MM\nM M M M\nM  M  M\nM     M\nM     M\nM     M\n");
+   }
+   public static void letterI(){
+   
+   }
+   public static void letterP(){
+   
+   }
+   public static void TwoISS(){
+   
+   }
+}
