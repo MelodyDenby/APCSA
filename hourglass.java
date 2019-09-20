2aecc045c6430bef55368ba3c57f2dcd4b1e951e
MelodyDenby <20014022@hartlandschools.us>
2019-09-20 07:42:35 -0400

    Create hourglass.java

---
+++ hourglass.java
@@ -0,0 +1,11 @@
+public class hourglass{
+   public static final int LINE=4; {
+      for(int i=1; i<=LINE; i++){
+         for(int j=1;j<=i;j++){
+            System.out.print("-");
+            }
+         }
+      }
+   public static void main(String[] args){
+      }
+   }
